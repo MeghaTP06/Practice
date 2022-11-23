508552e71867fe743e1be406953cbcf72c53e67f
admin <admin@DESKTOP-MR0E3M3>
2022-11-23 15:18:33 -0500

    framework15

--- src/test/java/pgm1.java
+++ src/test/java/pgm1.java
@@ -1,11 +1,18 @@
-
+import org.testng.annotations.Test;
 
 public class pgm1 
 {
-	public static void main(String[] args) 
+	@Test
+	public void script1()
 	{
-		System.out.println("p1");
-		
+		System.out.println("Script1");
 	}
+	
+	@Test
+	public void script2()
+	{
+		System.out.println("Script2");
+	}
+		
 
 }
